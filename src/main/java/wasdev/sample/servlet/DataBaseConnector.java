2e4ab67a4f9efed23cafbaa61123546da1750463
mortenkrarupsigaard <mortenkrarupsigaard@10.42.24.76>
2017-03-09 10:33:20 +0100

    old vers

--- src/main/java/wasdev/sample/servlet/DataBaseConnector.java
+++ src/main/java/wasdev/sample/servlet/DataBaseConnector.java
@@ -4,23 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Properties;
 
-import javax.annotation.Resource;
-import javax.sql.DataSource;
-
 
 public class DataBaseConnector {
-
-	@Resource(lookup = "jdbc/<strong>mydb</strong>")
+   
    public static java.sql.Connection Connect(){
 
 		String connectionUrl = "jdbc:postgresql://echo-01.db.elephantsql.com:5432/mvyxtiao";
 		String userName = "mvyxtiao";
 		String password = "hWaNiH-BX20jn6DS4cytcb5M9fgWdQAc";
 	   Connection con = null;
-       try{    	   
-            //Class.forName("org.postgresql.Driver"); 
-    	    DataSource myDataSource = null;
-            con = myDataSource.getConnection();//java.sql.DriverManager.getConnection(connectionUrl,userName,password);
+       try{
+            Class.forName("org.postgresql.Driver"); 
+            con = java.sql.DriverManager.getConnection(connectionUrl,userName,password);
             if(con!=null) System.out.println("Connection Successful!");           
             
        }catch(Exception e){
